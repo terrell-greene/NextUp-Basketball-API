974ab5153c78e9f8d97ad63b20b8167331e93e4e
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-14 21:56:40 -0600

    added password and upload scalars

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
-import { decorateType } from 'nexus'
+import { decorateType, asNexusMethod } from 'nexus'
 import { GraphQLDate, GraphQLDateTime } from 'graphql-iso-date'
+import { GraphQLUpload } from 'graphql-upload'
 
 export const GQLDate = decorateType(GraphQLDate, {
   rootTyping: 'Date',
@@ -11,7 +12,10 @@ export const GQLDateTime = decorateType(GraphQLDateTime, {
   asNexusMethod: 'datetime'
 })
 
+export const UploadScalar = asNexusMethod(GraphQLUpload, 'upload')
+
 export * from './query'
 export * from './model'
 export * from './enum'
 export * from './input'
+export * from './scalar'
