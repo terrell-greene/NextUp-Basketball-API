d3144ea919cc69d3a637075ffa346490f91b4dca
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2020-01-04 22:07:14 +0000

    added createCourt and deleteSuggestedCourt mutations

--- src/types/mutation/court.mutation.ts
+++ src/types/mutation/court.mutation.ts
@@ -1,4 +1,11 @@
 import { mutationField, arg } from 'nexus'
+import { DeleteSuggestedCourtError, ServerError } from '../../errors'
+import * as NodeGeocoder from 'node-geocoder'
+
+const geocoder = NodeGeocoder({
+  provider: 'google',
+  apiKey: 'AIzaSyClP9cgWUgxVeGQAo1we3-kFqXqwdXRvTE'
+})
 
 export const suggestCourt = mutationField('suggestCourt', {
   type: 'SuggestedCourt',
@@ -7,5 +14,86 @@ export const suggestCourt = mutationField('suggestCourt', {
   },
   resolve: async (_, { input }, { db }) => {
     return db.SuggestedCourt.create(input)
+  }
+})
+
+export const deleteSuggestedCourt = mutationField('deleteSuggestedCourt', {
+  type: 'Boolean',
+  args: {
+    input: arg({ type: 'DeleteSuggestedCourtInput', required: true })
+  },
+  resolve: async (_, { input }, { db }) => {
+    const { suggestedCourtId } = input
+
+    const deletedSuggestedCourt = await db.SuggestedCourt.findByIdAndDelete(
+      suggestedCourtId
+    )
+
+    if (!deletedSuggestedCourt) {
+      throw new DeleteSuggestedCourtError({
+        data: { suggestedCourtId: 'No suggested court with that id exists' }
+      })
+    }
+    return true
+  }
+})
+
+export const createCourt = mutationField('createCourt', {
+  type: 'Court',
+  args: {
+    input: arg({ type: 'CreateCourtInput', required: true })
+  },
+  resolve: async (_, { input }, { db }) => {
+    const {
+      suggestedCourtId,
+      name,
+      address,
+      phone,
+      numberOfCourts,
+      courtType,
+      timeZone
+    } = input
+
+    try {
+      const geoResult = await geocoder.geocode(address)
+      const {
+        latitude,
+        longitude,
+        streetNumber,
+        streetName,
+        city,
+        administrativeLevels,
+        state,
+        zipcode,
+        country
+      } = geoResult[0]
+
+      const court = await db.Court.create({
+        name,
+        numberOfCourts,
+        phone,
+        type: courtType,
+        address: {
+          streetNumber,
+          street: streetName,
+          city,
+          state: state ? state : administrativeLevels?.level1short,
+          zipCode: zipcode,
+          country,
+          timeZone
+        },
+        coords: {
+          latitude,
+          longitude
+        }
+      })
+
+      await db.SuggestedCourt.findByIdAndDelete(suggestedCourtId)
+
+      return court
+    } catch (error) {
+      console.error(error)
+      throw ServerError()
+    }
   }
 })
